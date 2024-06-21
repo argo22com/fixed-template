40b731c0f6a08d256c3255a533cbf01ed884a1bf
Matej Malicky <matej.malicky@gmail.com>
2024-06-21 10:36:15 +0200

    SPFYE/182 Title and metafield attributes
    - change product title dynamically

--- assets/critical-global.js
+++ assets/critical-global.js
@@ -422,12 +422,13 @@ class DrawerFixed extends HTMLElement {
   }
 }
 
-// FIXME done like this due to unknown error in this file. Should be done as 
+// FIXME done like this due to unknown error in this file. Should be done as web component
 var radioButtons = document.querySelectorAll('input.product-form__input__radio');
 
 radioButtons.forEach((radioButton) => {
   radioButton.addEventListener('change', (event) => {
     document.querySelector('truncate-text .truncate-text__content').innerHTML = event.target.getAttribute('data-desc')
+    document.querySelector('.product__title h1').innerHTML = event.target.getAttribute('data-title')
   })
 })
 
