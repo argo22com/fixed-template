0acd9d935b89de766ff18c0b3f30be75fa3d97fe
Adam Valalský <valalskya@magexo.cz>
2024-07-07 14:41:58 +0200

    feat: swiper breakpoints

--- assets/timeline.js
+++ assets/timeline.js
@@ -3,13 +3,31 @@ if (!customElements.get('timeline-section')) {
         constructor() {
             super();
 
-            const swiper = new Swiper('.swiper-container', {
+            new Swiper('.swiper-container', {
+                loop: false,
+                autoplay: false,
                 navigation: {
                     nextEl: '.swiper-button-next',
                     prevEl: '.swiper-button-prev',
-                }
+                },
+                breakpoints: {
+                    0: {
+                        slidesPerView: 1,
+                    },
+                    480: {
+                        slidesPerView: 2,
+                    },
+                    768: {
+                        slidesPerView: 3,
+                    },
+                    1023: {
+                        slidesPerView: 4,
+                    },
+                    1300: {
+                        slidesPerView: 5,
+                    },
+                },
             });
-            console.log(swiper);
         }
     }
 
